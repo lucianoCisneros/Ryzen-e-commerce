dfe617b39252cc3cc2110caf9df6822138f7b41e
lucianoCisneros <lucianocisneros22@gmail.com>
2020-07-03 18:53:23 -0300

    Validaciones de registro funcionando

--- website/controllers/registerController.js
+++ website/controllers/registerController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const db = require('../database/models');
+const DB = require('../database/models');
 let { check, validationResult, body } = require("express-validator");
 
 const registerController = {
@@ -7,12 +7,12 @@ const registerController = {
         res.render("register")
     },
     register: (req, res) => {
-        let errors = validationResult(req);
+        const errors = validationResult(req);
         let newUser = req.body;
 
         //Objeto que se va a guardar en base de datos
 
-        if(errors.isEmpty){
+        if(errors.isEmpty()){
             delete newUser.retype;
             newUser.userName = req.body.username;
             newUser.name = req.body.name;
@@ -21,23 +21,9 @@ const registerController = {
             newUser.email = req.body.email;
             newUser.password = bcrypt.hashSync(newUser.password, 15);
 
-            let usuarioEncontrado = () => {
-                let user = req.body;
-                return db.User.userName === user.username;
-            }
-            let emailEncontrado = () => {
-                let user = req.body;
-                return db.User.email === user.email;
-            }
-
-            if(usuarioEncontrado){
-                return res.render('register', { errors: [{ msg: "El nombre de usuario ingresado ya está en uso" }] })
-            } else if (emailEncontrado){
-                return res.render('register', { errors: [{ msg: "El email ingresado ya está en uso" }] })
-            } else {
-                db.User.create(newUser)
+            DB.User.create(newUser)
                     .then(() => res.redirect('/login'));
-            }
+
         } else {
             res.render('register', { errors: errors.errors });
         }
--- website/middlewares/registerMiddleware.js
+++ website/middlewares/registerMiddleware.js
@@ -1,34 +1,59 @@
 const { check , body} = require("express-validator");
+const DB = require('../database/models');
 
 let registerMiddleware = [
-    check('username')
-        .isLength({min:6})
-        .withMessage('El usuario debe tener por lo menos 6 caracteres'),
-    check('email')
+    body('username')
+        .isLength({min: 4})
+        .withMessage('El usuario debe contener minimo 5 caracteres')
+        .notEmpty()
+        .withMessage('Este campo es obligatorio')
+        .custom(value => {
+            return DB.User.findOne({
+                where: {
+                    userName: value
+                }
+            })
+                .then(function (resultado) {
+                    if (resultado) {
+                        return Promise.reject('El usuario ingresado se encuentra en uso')
+                    }
+                })
+        }),
+    body('name')
+        .notEmpty()
+        .withMessage('Este campo es obligatorio'),
+    body('lastname')
+        .notEmpty()
+        .withMessage('Este campo es obligatorio'),
+    body('email')
+        .notEmpty()
+        .withMessage('Este campo es obligatorio').bail()
         .isEmail()
-        .withMessage('Ingrese un tipo de email valido'),
-    check('password')
-        .isLength({min:8})
-        .withMessage('La contraseña debe tener por lo menos 8 caracteres'),
-    /* body('username').custom(function(value){
-        let usersFilePath = path.join(__dirname, '/../data/users.json');
-        let users;
+        .withMessage('Debes ingresar un email valido').bail()
+        .custom(value => {
+            return DB.User.findOne({
+                where: {
+                    email: value
+                }
+            })
+                .then(function (resultado) {
+                    if (resultado) {
+                        return Promise.reject('El email ingresado se encuentra en uso')
+                    }
+                })
+        }),
 
-        if(usersFilePath = "") {
-            users = []
-        }
-        else {
-            users = JSON.parse(users)
-        }
-
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].username == value) {
-                return false
-            }
-        }
-
-        return true;
-    }).withMessage('El nombre de usuario ya está en uso') */
+    body('password')
+        .notEmpty()
+        .withMessage('Este campo es obligatorio').bail()
+        .isLength({
+            min: 8
+        }).withMessage('La contraseña debe tener por lo menos 8 caracteres').bail()
+        .custom((value, {
+            req
+        }) => req.body.password == req.body.retype).withMessage('Las passwords no coinciden'),
+    body('retype')
+        .notEmpty().withMessage('Este campo es obligatorio')
 ];
 
 module.exports = registerMiddleware;
