656c050297c6bda6c27da8484e473dc392e394ab
nmolina95 <ignaciomolina.95@hotmail.com>
2020-07-25 21:20:03 -0300

    Algunos cambios en home.

--- website/public/js/home.js
+++ website/public/js/home.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', () => {
-    let container = document.querySelector('.secondContainer')
+    let container = document.querySelector('.secondContainer,.row')
     let firstContainer = document.querySelector('.firstContainer')
 
     window.addEventListener('scroll', function(e){
         container.style.animation = 'fadeIn 8s ease 0s';
+        console.log('done')
     })
 })
