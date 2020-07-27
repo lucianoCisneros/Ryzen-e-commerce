3b4f6bd98e293377afa89cf24a751db3ae160f79
lucianoCisneros <lucianocisneros22@gmail.com>
2020-07-27 18:17:52 -0300

    correccion de erorres en rutas

--- website/routes/profile.js
+++ website/routes/profile.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const profileController = require('../controllers/profileController');
 
 router.get('/', usersController.profile);
+router.get('/editar-producto', profileController.formEdit);
+router.post('/editar-producto', profileController.redirectToProduct);
+router.get('/permisos', profileController.formPermisos);
+router.post('/permisos', profileController.updatePermisos);
 
 module.exports = router;
