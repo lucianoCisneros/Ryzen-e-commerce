bd32e01c21886d1220d088eb829f6db9622b351d
nmolina95 <ignaciomolina.95@hotmail.com>
2020-07-31 03:02:44 -0300

    Se agregó en la lista la posibilidad de volver a la selección de categorías con un botón que se implementó junto a su lógica. Cambios estéticos en producto (títulos y textos acompañantes centrados, imágenes y video con sombra de caja).

--- website/public/js/profile.js
+++ website/public/js/profile.js
@@ -3,23 +3,36 @@ window.addEventListener('load', () => {
     let categoriasNombre = document.querySelectorAll('.categorieDiv span');
     let productos1 = document.querySelectorAll('.productsJS');
     let editOption = document.querySelector('.name p');
+    let returnOption = document.querySelector('.name span')
 
-    var categoryClick
+    let categoryClick
     for (let i = 0; i < categorias.length; i++) {
-        categorias[i].addEventListener('click', (e) => {
+        categorias[i].addEventListener('click', () => {
                 categorias.forEach(e => {
-                    e.classList.add('productCategory');
-                    categoryClick = i;
+                    e.classList.add('noDisplay');
                 })
+            categoryClick = i;
 
             for (let y = 0; y < productos1.length; y++) {
                 let hasClass = productos1[y].classList.contains('productCategory' + (categoryClick + 1));
                 if (hasClass) {
-                    productos1[y].classList.remove('productCategory')
+                    productos1[y].classList.remove('noDisplay')
                 }
             }
 
             editOption.innerHTML += ' - ' + categoriasNombre[i].innerHTML;
-    })
+            returnOption.classList.remove('noDisplay')
+        })
     }
+
+    returnOption.addEventListener('click', () => {
+        categorias.forEach(e => {
+            e.classList.remove('noDisplay');
+        })
+        productos1.forEach(e => {
+            e.classList.add('noDisplay');
+        })
+        editOption.innerHTML = 'Seleccione la opción a editar';
+        returnOption.classList.add('noDisplay');
+    })
 })
